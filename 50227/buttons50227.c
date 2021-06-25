b9f1436bf8d5484657d28dd6289d641e334eac4a
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-06-25 22:05:06 +0200

    All buttons mapped now; init of displays still weird; LEDs not yet decoded.

--- 50227/buttons50227.c
+++ 50227/buttons50227.c
@@ -82,19 +82,49 @@ unsigned char butt_scan()
 check_specials:
 	DDR(KEY_R) &= ~KEY_R_MASK;
 
-	// check the special button wired between col 2 and col 3.
-	//
-	SET(KEY_C3,0);
-	OUTPUT(KEY_C3); /* after disabling pull-up/setting low */
+        SET(KEY_C2,0);
+        OUTPUT(KEY_C2); /* after disabling pull-up/setting low */
+	SET(KEY_C1,1);
+	SET(KEY_C3,1);
+        _delay_us(2);
+        unsigned char exec = READ(KEY_C1);
+        unsigned char l3 = READ(KEY_C3);
 
+        INPUT(KEY_C2); 
+        SET(KEY_C2,1);  /* after setting back to input! (prevent shorts) */
+
+	if (exec == 0)
+		return 19;
+	if (l3 == 0)
+		return 20;
+
+	SET(KEY_R1,0);
+	OUTPUT(KEY_R1);
+
+	SET(KEY_R0,1);
+	SET(KEY_R2,1);
 	_delay_us(2);
-	v = READ(KEY_C2);
+	unsigned char v06 = READ(KEY_R0); // for '06'
+	unsigned char v22 = READ(KEY_R2); // for '22'
+	INPUT(KEY_R1); 
+	SET(KEY_R0,0);
+	SET(KEY_R2,0);
 
-	INPUT(KEY_C3); 
-	SET(KEY_C3,1);  /* after setting back to input! (prevent shorts) */
-
-	if (v == 0)
+	if (v06 == 0)
 		return 16;
+	if (v22 == 0)
+		return 17;
+
+	SET(KEY_R2,0);
+	OUTPUT(KEY_R2);
+	SET(KEY_R3,1);
+	_delay_us(2);
+	unsigned char corr = READ(KEY_R3); // for '22'
+	INPUT(KEY_R2); 
+	SET(KEY_R3,0);
+
+	if (corr == 0)
+		return 18;
 
 	return 255;
 };
@@ -105,11 +135,23 @@ const char * butt_scan2label(unsigned char at) {
 		{ "18R", "18L", "09", "HELI", },
 	        { "L1", "36L", "36R", "27", },
 		{ "R2", "R1", "18C", "24", },
-		{ "04", "R3", "L2", "UP" },
+		{ "04", "R3", "L2", "36C" },
 	};
-	if (at < 15)
+	static const char *_collabels[] = {
+		"06", "22", "CORR", "EXE", "L3", 
+	};
+
+	if (at < 16)
 		return _labels[ at / KEY_COLS][ at % KEY_COLS ];
-	if (at == 16)
-		return "L3";
-	return "";
+
+	if (at >= 15 && at < 21)
+		return _collabels[ at - 16 ];
+
+	static char out[5];
+	out[0] = '0' + at/100;
+	out[1] = '0' + (at/10) % 10;
+	out[2] = '0' + at % 10;
+	out[3] = '?';
+	out[4] = 0;
+	return out;
 }
